0dfaa01b88597fad93102ccbebc5ddb6760efd84
Luke Hamilton <luke@lukehamilton.me>
2014-05-07 16:50:39 +1000

    Removed old code and added a test for checking cart state

--- app/models/spree/promotion/rules/country.rb
+++ app/models/spree/promotion/rules/country.rb
@@ -3,10 +3,7 @@
     module Rules
       class Country < Spree::PromotionRule
         has_and_belongs_to_many :country, class_name: '::Spree::Country', join_table: 'spree_countries_promotion_rules', foreign_key: 'promotion_rule_id'
-
-        #MATCH_POLICIES = %w(any all)
-        #preference :match_policy, :string, default: MATCH_POLICIES.first
-
+        
         def eligible_countries
           country
         end
@@ -16,11 +13,7 @@
         end
 
         def eligible?(order, options = {})    
-          #return true if eligible_countries.empty?       
-          puts "eligible_countries.include?(order.shipping_address.country)"
-          puts "Order state: #{order.state}"
-          puts caller.join("\n")
-          eligible_countries.include?(order.shipping_address.country)
+          eligible_countries.include?(order.shipping_address.country) if order.state != 'cart'
         end
 
         def country_ids_string
